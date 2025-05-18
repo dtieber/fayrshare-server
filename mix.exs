4dceeb465a7ecf13af5da244e344ebc091c04d1d
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-05-18 12:39:56 +0200

    feat: init elixir application
    using `mix new fayrshare --sup`

---
+++ mix.exs
@@ -0,0 +1,24 @@
+defmodule Fayrshare.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :fayrshare,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Fayrshare.Application, []}
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
