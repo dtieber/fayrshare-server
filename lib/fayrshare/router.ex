939e75cec0c375f79e6aaf047880f2b6fe32b0b5
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-05-24 13:33:11 +0200

    feat: remove index route

--- lib/fayrshare/router.ex
+++ lib/fayrshare/router.ex
@@ -6,10 +6,6 @@ defmodule Fayrshare.Router do
   plug(:match)
   plug(:dispatch)
 
-  match "/", via: :get do
-    send_resp(conn |> put_resp_content_type("application/json"), 200, Jason.encode!(%{hello: "fayrshare"}))
-  end
-
   match _ do
     send_resp(conn, 404, "Not found")
   end
--- test/router_test.exs
+++ test/router_test.exs
@@ -4,13 +4,4 @@ defmodule RestApiTest.Router do
 
   @opts Fayrshare.Router.init([])
 
-  test "index route returns valid json" do
-    conn = conn(:get, "/")
-
-    conn = Fayrshare.Router.call(conn, @opts)
-
-    assert conn.state == :sent
-    assert conn.status == 200
-    assert Jason.decode(conn.resp_body) == {:ok, %{"hello" => "fayrshare"}}
-  end
 end
