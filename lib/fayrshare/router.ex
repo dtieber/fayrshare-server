fdda63f780b0a905d82bba373adbad9989362705
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-05-24 13:34:23 +0200

    feat: unify response handling

--- lib/fayrshare/router.ex
+++ lib/fayrshare/router.ex
@@ -7,6 +7,18 @@ defmodule Fayrshare.Router do
   plug(:dispatch)
 
   match _ do
-    send_resp(conn, 404, "Not found")
+    response = {:not_found, %{error: "Route is not mapped"}}
+    response |> handle_response(conn)
+  end
+
+  defp handle_response(response, conn) do
+    %{code: code, payload: payload} =
+      case response do
+        {:ok, payload} -> %{code: 200, payload: payload}
+        {:not_found, payload} -> %{code: 404, payload: %{error: "Not found: #{payload.error}"}}
+        {:error, _} -> %{code: 500, payload: %{error: "An error occurred"}}
+      end
+
+    send_resp(conn |> put_resp_content_type("application/json"), code, Jason.encode!(payload))
   end
 end
