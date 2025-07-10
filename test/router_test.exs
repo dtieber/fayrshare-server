6f71fbd2423a47abb2fd443e311388767ebb23b0
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-07-10 15:55:14 +0200

    test: update router test to initialise data

--- test/router_test.exs
+++ test/router_test.exs
@@ -1,10 +1,16 @@
 defmodule RestApiTest.Router do
-  use ExUnit.Case, async: true
+  use RepoCase, async: true
   import Plug.Test
 
   @opts Fayrshare.Router.init([])
 
   test "/expense-groups returns list of expense-groups" do
+    {:ok, %ExpenseGroup{id: id1}} =
+      Services.ExpenseGroupService.add_expense_group(%{name: "Expense Group One"})
+
+    {:ok, %ExpenseGroup{id: id2}} =
+      Services.ExpenseGroupService.add_expense_group(%{name: "Expense Group Two"})
+
     conn = conn(:get, "/expense-groups")
 
     conn = Fayrshare.Router.call(conn, @opts)
@@ -15,8 +21,8 @@ defmodule RestApiTest.Router do
     assert Jason.decode(conn.resp_body) ==
              {:ok,
               [
-                %{"id" => 1, "name" => "Expense Group One"},
-                %{"id" => 2, "name" => "Expense Group Two"}
+                %{"id" => id1, "name" => "Expense Group One"},
+                %{"id" => id2, "name" => "Expense Group Two"}
               ]}
   end
 
