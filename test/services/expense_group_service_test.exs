23d3f5db1e66ae569e218c424467a3b6fe107364
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-07-10 15:55:15 +0200

    test: switch from repository tests to service tests
    the repositories are mainly wrapping Ecto with some config. hence, it makes more sense to run integration tests between services and the database.

---
+++ test/services/expense_group_service_test.exs
@@ -0,0 +1,15 @@
+defmodule Repositories.ExpenseGroupServiceTest do
+  use RepoCase
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repositories.ExpenseGroupRepository)
+  end
+
+  test "adds an expense group" do
+    {:ok, %ExpenseGroup{id: id, name: name}} =
+      Services.ExpenseGroupService.add_expense_group(%{name: "Test Expense Group"})
+
+    assert id != nil
+    assert name == "Test Expense Group"
+  end
+end
