ec86625910da2c0598e0adbfc8fcf261d6fe0834
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-07-10 15:55:08 +0200

    feat: add changeset for expense groups

--- lib/fayrshare/model/expense_group.ex
+++ lib/fayrshare/model/expense_group.ex
@@ -1,9 +1,16 @@
 defmodule ExpenseGroup do
   use Ecto.Schema
+  import Ecto.Changeset
 
   @derive {Jason.Encoder, except: [:__meta__, :__struct__]}
 
   schema "expense_groups" do
     field :name, :string
+  end
+
+  def changeset(expense_group, params \\ %{}) do
+    expense_group
+    |> cast(params, [:name])
+    |> validate_required([:name])
   end
 end
