f67ead16186fffa1482324539bbe35eabee3a156
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-07-10 15:55:08 +0200

    feat: add method to create expense groups

--- lib/fayrshare/services/expense_group_service.ex
+++ lib/fayrshare/services/expense_group_service.ex
@@ -1,4 +1,17 @@
 defmodule Services.ExpenseGroupService do
+  def add_expense_group(data) do
+    changeset = ExpenseGroup.changeset(%ExpenseGroup{}, data)
+
+    case changeset.valid? do
+      false ->
+        {:invalid, "Invalid data"}
+
+      true ->
+        response = get_repository().insert(changeset)
+        response
+    end
+  end
+
   def get_all_expense_groups do
     expense_groups = get_repository().all(ExpenseGroup)
     {:ok, expense_groups}
