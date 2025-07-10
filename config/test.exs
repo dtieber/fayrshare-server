3880b10f2cb452aba0dbdf77a7ae721082396b35
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-07-10 15:55:11 +0200

    feat: add configuration for test repositories

--- config/test.exs
+++ config/test.exs
@@ -2,3 +2,12 @@ import Config
 
 config :fayrshare,
   port: 4000
+
+config :fayrshare, Repositories.ExpenseGroupRepository,
+  database: "fayrshare_test",
+  username: "myuser",
+  password: "mypassword",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+config :fayrshare, ecto_repos: [Repositories.ExpenseGroupRepository]
