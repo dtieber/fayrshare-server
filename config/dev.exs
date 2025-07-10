06ce32f184a2052cd512763262251083afdb78d6
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-07-10 15:55:01 +0200

    chore: add dev database connection details to config

--- config/dev.exs
+++ config/dev.exs
@@ -4,3 +4,11 @@ config :fayrshare,
   port: 4000
 
 config :fayrshare, :expense_group_repository, Repositories.ExpenseGroupRepository
+
+config :fayrshare, Repositories.ExpenseGroupRepository,
+  database: "fayrshare",
+  username: "myuser",
+  password: "mypassword",
+  hostname: "localhost"
+
+config :fayrshare, ecto_repos: [Repositories.ExpenseGroupRepository]
